4faeeb0c6d25f0b1ff58d67c8969801c74c6b5ea
Ezzyezn <art.samerui@gmail.com>
2025-11-26 15:02:12 +0100

    Dodaj pliki projektów.

---
+++ AppArtem/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace AppArtem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string textTwo;
+            //Console.WriteLine("Wprowadz dowolny text:");
+            //string text = Console.ReadLine();
+            //int lenghtText = text.Length;
+            //Console.WriteLine("Wprowadź 'UP' albo 'LOW' żeby miec duże albo małę litery");
+            //string chose = Console.ReadLine();
+            //Console.WriteLine("Czy Pierwsza litera ma być duża? Tak/Nie");
+            //string choseTwo = Console.ReadLine();
+            //if (choseTwo == "Tak")
+            //{
+            //    textTwo = text.Substring(0,1).ToUpper();
+            //}
+            //else
+            //{
+            //    textTwo = text.Substring(0,1);
+            //}
+
+            //if (chose == "UP")
+            //{
+            //    Console.WriteLine(textTwo.ToUpper() + text.Substring(1).ToUpper());
+            //}
+            //else if (chose == "LOW")
+            //{
+            //    Console.WriteLine(textTwo+ text.Substring(1).ToLower());
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ERROR");
+            //}
+            //Console.WriteLine(text.Length);
+
+
+            //Console.WriteLine("Wprowadz dowolny text");
+            //string text = Console.ReadLine();
+            //string TrimText = text.Trim();
+            //int lenghtText = TrimText.Length;
+            //string FirstLetter = TrimText.Substring(0, 1);
+            //string LastLetter = TrimText.Substring(lenghtText - 1);
+            //Console.WriteLine("Text: " + TrimText + "\n" + "Długość: "+ lenghtText + "\n"+ "Pierwsza litera: " + FirstLetter + "\n"+ "Ostatnia litera: " + LastLetter);
+
+
+
+            //Console.WriteLine("Wprowadz dowolny text");
+            //string text = Console.ReadLine();
+            //Console.WriteLine("Wybierz literę, Ktorą chcesz policzyć");
+            //string Letter = Console.ReadLine();
+            //int Count = 0;
+            //for (int i = 0; i < text.Length; i++)
+            //{
+            //    if (text[i].ToString() == Letter){ 
+            //        Count++;
+            //     }
+            //}
+            //Console.WriteLine("Wprowadzony text: " + text + "\n" + "Liczba liter: " + Count);
+
+
+
+
+            //Console.WriteLine("Wprowadz dowolny text");
+            //string text = Console.ReadLine();
+            //Console.WriteLine("Wybierz znak, który bedzie ustawiony zamiast spacji");
+            //string symbol = Console.ReadLine();
+            //Console.WriteLine("Wybierz literę ktorą chcesz zamienic na jakis inny znak/symbol");
+            //string letter = Console.ReadLine();
+            //Console.WriteLine("Wybierz znak, który bedzie ustawiony zamiast litery: " + letter);
+            //string symbolTwo = Console.ReadLine();
+            //string ReplaceText = text.Replace(" ", symbol);
+            //string ReplaceTextTwo = ReplaceText.Replace(letter, symbolTwo);
+            //Console.WriteLine(ReplaceTextTwo);
+        }
+    }
+}
